b6688c271a21c60d74b275dd11e5053177861c08
Benjamin Scher Purcell <benjapurcell@gmail.com>
2016-08-10 08:41:02 -0500

    make newquine smaller

--- newquine.c
+++ newquine.c
@@ -8,7 +8,6 @@ main(void)
 {
 	char *c;
 
-	printf("#include <stdio.h>\n#include <stdlib.h>\n\nextern char *prog;\n\nint\nmain(void)\n{\n\tchar *c;\n\n\t");
 	printf("%s\n", prog);
 	printf("char *prog = \"");
 	for(c = prog; *c != 0; c++) switch(*c) {
@@ -32,4 +31,4 @@ main(void)
 	exit(0);
 }
 
-char *prog = "printf(\"#include <stdio.h>\\n#include <stdlib.h>\\n\\nextern char *prog;\\n\\nint\\nmain(void)\\n{\\n\\tchar *c;\\n\\n\\t\");\n\tprintf(\"%s\\n\", prog);\n\tprintf(\"char *prog = \\\"\");\n\tfor(c = prog; *c != 0; c++) switch(*c) {\n\tdefault:\n\t\tputchar(*c);\n\t\tbreak;\n\tcase '\\\\':\n\t\tprintf(\"\\\\\\\\\");\n\t\tbreak;\n\tcase '\\n':\n\t\tprintf(\"\\\\n\");\n\t\tbreak;\n\tcase '\\t':\n\t\tprintf(\"\\\\t\");\n\t\tbreak;\n\tcase '\"':\n\t\tprintf(\"\\\\\\\"\");\n\t\tbreak;\n\t}\n\tprintf(\"\\\";\\n\");\n\texit(0);\n}\n";
+char *prog = "#include <stdio.h>\n#include <stdlib.h>\n\nextern char *prog;\n\nint\nmain(void)\n{\n\tchar *c;\n\n\tprintf(\"%s\\n\", prog);\n\tprintf(\"char *prog = \\\"\");\n\tfor(c = prog; *c != 0; c++) switch(*c) {\n\tdefault:\n\t\tputchar(*c);\n\t\tbreak;\n\tcase '\\\\':\n\t\tprintf(\"\\\\\\\\\");\n\t\tbreak;\n\tcase '\\n':\n\t\tprintf(\"\\\\n\");\n\t\tbreak;\n\tcase '\\t':\n\t\tprintf(\"\\\\t\");\n\t\tbreak;\n\tcase '\"':\n\t\tprintf(\"\\\\\\\"\");\n\t\tbreak;\n\t}\n\tprintf(\"\\\";\\n\");\n\texit(0);\n}\n";
