b21e5aa80ea0ce88b08927226f038b94b1ab5ad8
Benjamin Purcell <bpurcell@backstopsolutions.com>
2016-08-13 00:37:32 -0500

    cleaner rotquines

--- rotquinegen.c
+++ rotquinegen.c
@@ -8,12 +8,11 @@ main(void)
 {
 	unsigned char *c;
 
-	printf("%s\n", prog);
-	printf("char prog[] = {");
+	printf("%s", prog);
 	for(c = (unsigned char *)prog; *c != 0; c++)
 		printf(" %d,", *c + 128);
 	printf(" 0 };\n");
 	exit(0);
 }
 
-char prog[] = "#include <stdio.h>\n#include <stdlib.h>\n\nextern char prog[];\n\nint\nmain(void)\n{\n\tunsigned char *c;\n\n\tfor(c = (unsigned char *)prog; *c != 0; c++)\n\t\t*c += 128;\n\tprintf(\"%s\\n\", prog);\n\tprintf(\"char prog[] = {\");\n\tfor(c = (unsigned char *)prog; *c != 0; c++)\n\t\tprintf(\" %d,\", *c + 128);\n\tprintf(\" 0 };\\n\");\n\texit(0);\n}\n";
+char prog[] = "#include <stdio.h>\n#include <stdlib.h>\n\nextern char prog[];\n\nint\nmain(void)\n{\n\tunsigned char *c;\n\n\tfor(c = (unsigned char *)prog; *c != 0; c++)\n\t\t*c -= 128;\n\tprintf(\"%s\", prog);\n\tfor(c = (unsigned char *)prog; *c != 0; c++)\n\t\tprintf(\" %d,\", *c + 128);\n\tprintf(\" 0 };\\n\");\n\texit(0);\n}\n\nchar prog[] = {";
